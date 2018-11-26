5eadf9d0e192abd628725ea43cfa17ce5d6145a1
Luhiriri Guillaume <guillaume.luhiriri@qwentes.it>
2018-11-26 16:54:05 +0100

    About me Button : Final tweaks

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ var thumbnail = document.getElementById('thumbnail');
 thumbnail.addEventListener('click',openDetails);
 
 function openDetails(){
+  var thubnailImg = document.getElementById('thumbnailImg');
   var myDetails = document.getElementById('myDetails');
   myDetails.classList.toggle('openMyDetails');
+  thubnailImg.classList.toggle('addFocus');
 }
 
