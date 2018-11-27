29ba59d819e1fda65418f82db4fc9a027a3c14b6
Luhiriri Mudekereza Guillaume <guillaumeluhiriri@gmail.com>
2018-11-27 02:29:26 +0100

    Create hamburger Menu
    And fixed bugged

--- main.js
+++ main.js
@@ -5,8 +5,16 @@ var thumbnail = document.getElementById('thumbnail');
 thumbnail.addEventListener('click',openDetails);
 
 function openDetails(){
-  var thumbnailImg = document.getElementById('thumbnailImg');
   var myDetails = document.getElementById('myDetails');
   myDetails.classList.toggle('openMyDetails');
+}
+
+// About the Hamburger
+// Finding Humburger and define variable
+var hamburger = document.getElementById('hamburger');
+hamburger.addEventListener('click',openMenu);
+function openMenu(){
+  hamburger.classList.toggle('crossHamburger');
+  console.log('worked!');
 }
 
